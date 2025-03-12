d4008b2bbd8378641aa38d8dd2e98b9eb6053e1f
Neko <neko@ayaka.moe>
2025-03-12 15:18:00 +0800

    chore(deps): cleanup (#9)

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,15 @@ import type { UserModule } from './types'
 import { setupLayouts } from 'layouts-generated'
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
-
 import { routes } from 'vue-router/auto-routes'
 import App from './App.vue'
+
 import '@vue-flow/core/dist/style.css'
 import '@vue-flow/core/dist/theme-default.css'
 import '@vue-flow/controls/dist/style.css'
 import '@vue-flow/minimap/dist/style.css'
 import '@unocss/reset/tailwind.css'
+
 import './styles/main.css'
 import 'uno.css'
 
