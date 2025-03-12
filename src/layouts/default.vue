0a6b1a6d7d33060e411ee6798996fdacc5428c3d
liliumneko <30291104+liliumneko@users.noreply.github.com>
2025-03-13 03:19:53 +0800

    add toggle theme buttom and auto check theme

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -114,6 +114,10 @@ const toggleTheme = inject('toggleTheme', () => {})
           </Button>
         </DialogContent>
       </Dialog>
+      <Button class="rounded-md bg-white hover:bg-accent" @click="handleToggle">
+        <i v-if="isDark" class="i-carbon-moon text-xl text-gray-800" />
+        <i v-else class="i-carbon-sun bg-yellow-500 text-xl" />
+      </Button>
       <Button
         class="border border-gray-300 rounded-md bg-white px-2 transition-colors duration-200 dark:border-gray-500 dark:bg-white hover:bg-gray-200 dark:hover:bg-gray-300"
         @click="toggleTheme"
