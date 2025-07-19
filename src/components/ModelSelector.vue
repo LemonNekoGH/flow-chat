afd25a85294007c48fb845f9e6668ce1a736f486
LemonNeko <self@lemonneko.moe>
2025-07-20 00:32:43 +0800

    fix: remove vapor mode (#63)

--- src/components/ModelSelector.vue
+++ src/components/ModelSelector.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts" vapor>
+<script setup lang="ts">
 import { useEventListener } from '@vueuse/core'
 import { computed, ref, watch } from 'vue'
 import { useSettingsStore } from '~/stores/settings'
