ff4ac11a954ef28a4bebdbcb7f83d27973658294
LemonNeko <self@lemonneko.moe>
2025-08-28 15:44:11 +0800

    chore: bump deps (#74)

--- src/typed-router.d.ts
+++ src/typed-router.d.ts
@@ -24,4 +24,48 @@ declare module 'vue-router/auto-routes' {
     '/settings/': RouteRecordInfo<'/settings/', '/settings', Record<never, never>, Record<never, never>>,
     '/settings/modules/text-generation': RouteRecordInfo<'/settings/modules/text-generation', '/settings/modules/text-generation', Record<never, never>, Record<never, never>>,
   }
+
+  /**
+   * Route file to route info map by unplugin-vue-router.
+   * Used by the volar plugin to automatically type useRoute()
+   *
+   * Each key is a file path relative to the project root with 2 properties:
+   * - routes: union of route names of the possible routes when in this page (passed to useRoute<...>())
+   * - views: names of nested views (can be passed to <RouterView name="...">)
+   *
+   * @internal
+   */
+  export interface _RouteFileInfoMap {
+    'src/pages/index.vue': {
+      routes: '/'
+      views: never
+    }
+    'src/pages/[...all].vue': {
+      routes: '/[...all]'
+      views: never
+    }
+    'src/pages/chat/[id].vue': {
+      routes: '/chat/[id]'
+      views: never
+    }
+    'src/pages/settings/index.vue': {
+      routes: '/settings/'
+      views: never
+    }
+    'src/pages/settings/modules/text-generation.vue': {
+      routes: '/settings/modules/text-generation'
+      views: never
+    }
+  }
+
+  /**
+   * Get a union of possible route names in a certain route component file.
+   * Used by the volar plugin to automatically type useRoute()
+   *
+   * @internal
+   */
+  export type _RouteNamesForFilePath<FilePath extends string> =
+    _RouteFileInfoMap extends Record<FilePath, infer Info>
+      ? Info['routes']
+      : keyof RouteNamedMap
 }
