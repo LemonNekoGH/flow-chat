68bcaa3667c8dac278ae8164c3b4d01f09dc7f68
s3d-i <bing@sed-i.org>
2025-10-22 17:24:04 +0800

    feat(markdown): add copyable code/table components

--- uno.config.ts
+++ uno.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
   shortcuts: [
     ['btn', 'px-4 py-1 rounded inline-block bg-teal-700 text-white cursor-pointer !outline-none hover:bg-teal-800 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
+    ['copy-icon-btn', 'h-7 w-7 flex items-center justify-center rounded-full hover:bg-black/10 dark:hover:bg-white/10'],
     {
       'animate-accordion-up': 'accordion-up',
       'animate-accordion-down': 'accordion-down',
