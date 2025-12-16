74fae13f5e1f686b3e39e324f21e76b03a39d13e
Rin <shinohara-rin@users.noreply.github.com>
2025-12-17 01:49:03 +0800

    fix(schema): cascade delete rooms (#90)
    Co-authored-by: LemonNeko <self@lemonneko.moe>

---
+++ drizzle/0001_chubby_odin.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "messages" DROP CONSTRAINT "messages_room_id_rooms_id_fk";
+--> statement-breakpoint
+ALTER TABLE "messages" ADD CONSTRAINT "messages_room_id_rooms_id_fk" FOREIGN KEY ("room_id") REFERENCES "public"."rooms"("id") ON DELETE cascade ON UPDATE no action;
